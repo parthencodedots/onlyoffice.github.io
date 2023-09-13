d8a9334aa942b7a63aff411091a50699d1819715
Parth <parth.encodedots@gmail.com>
2023-09-13 11:03:39 +0530

    Update code.js

--- sdkjs-plugins/content/propact_dev/scripts/code.js
+++ sdkjs-plugins/content/propact_dev/scripts/code.js
@@ -117,9 +117,9 @@
                 if (!document.getElementById('btnCreateClause').classList.contains(disabledClass)) {
                     document.getElementById('btnCreateClause').classList.add(disabledClass);
                 }
-                if (!document.getElementById('divContractCreate').classList.contains('d-none')) {
-                    document.getElementById('divContractCreate').classList.add(disabledClass);
-                    document.getElementById('divContractLists').classList.remove(disabledClass);
+                if (!document.getElementById('divContractCreate').classList.contains(displayNoneClass)) {
+                    document.getElementById('divContractCreate').classList.add(displayNoneClass);
+                    document.getElementById('divContractLists').classList.remove(displayNoneClass);
                 }
             }
             if (!fDisableWhenPluginLoading) {
